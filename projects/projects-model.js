8ae8c49de354c7fff2171dd6a73456cd7f1ff046
LenWinkler <lenwinkler86@gmail.com>
2019-11-08 12:54:06 -0500

    wrote helper functions

---
+++ projects/projects-model.js
@@ -0,0 +1,46 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+    getProjects,
+    getTasks,
+    getResources,
+    addProject,
+    addTask,
+    addResource
+}
+
+function getProjects() {
+    return db('projects')
+};
+
+function getTasks() {
+    return db('tasks')
+};
+
+function getResources() {
+    return db('resources')
+};
+
+function addProject(project) {
+    return db('projects')
+    .insert(project, ['id', 'name', 'description', 'completed'])
+    .then(response => {
+        return response
+    })
+};
+
+function addTask(task) {
+    return db('tasks')
+    .insert(task, ['id', 'description', 'notes', 'completed', 'project_id'])
+    .then(response => {
+        return response
+    })
+};
+
+function addResource(resource) {
+    return db('resources')
+    .insert(resource, ['id', 'name', 'description', 'project_id'])
+    .then(response => {
+        return response
+    })
+};
